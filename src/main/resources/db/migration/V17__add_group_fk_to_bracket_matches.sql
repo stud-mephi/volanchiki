bc47b6752bef14251870db7632d013810f93822a
stud_mephi <aekochanova@gmail.com>
2026-05-25 23:19:06 +0300

    Delete src/main/resources/db/migration/V17__add_group_fk_to_bracket_matches.sql

--- src/main/resources/db/migration/V17__add_group_fk_to_bracket_matches.sql
+++
@@ -1,6 +0,0 @@
--- Добавляем внешний ключ для связи bracket_matches с группами
-ALTER TABLE bracket_matches
-    ADD CONSTRAINT fk_bracket_matches_group
-    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE SET NULL;
-
--- Триггер уже есть в V13, ничего не добавляем
