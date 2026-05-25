f4159b67773f004682ec1ae5ea9b1e5439f2443a
stud_mephi <aekochanova@gmail.com>
2026-05-25 13:08:07 +0300

    Add files via upload

---
+++ scr/main/java/com/badminton/tournament/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.badminton.tournament.controller;
+
+import com.badminton.tournament.dto.AuthRequestDTO;
+import com.badminton.tournament.dto.AuthResponseDTO;
+import com.badminton.tournament.dto.RegisterRequestDTO;
+import com.badminton.tournament.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody RegisterRequestDTO dto) {
+        return ResponseEntity.ok(userService.register(dto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody AuthRequestDTO dto) {
+        return ResponseEntity.ok(userService.login(dto));
+    }
+}
